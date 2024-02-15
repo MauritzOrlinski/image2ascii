407bbb04088eae8ef83b56ac34a887d82892006b
mauritzorlinski <mauorlin@gmail.com>
2024-02-15 09:26:39 +0100

    added a colorful output modus and fixed a bug when inferring the height from the given width

--- image_to_text/image_reader.py
+++ image_to_text/image_reader.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 
+
 def rgb_to_brightness(image):
     return np.dot(image[...,:3], [0.299, 0.587, 0.114])
 
@@ -8,5 +9,5 @@ def rgb_to_brightness(image):
 def get_image(path):
     image = Image.open(path)
     image = np.asarray(image)
-    image = rgb_to_brightness(image)
-    return image
+    image_brightness = rgb_to_brightness(image)
+    return image, image_brightness
--- image_to_text/image_to_text.py
+++ image_to_text/image_to_text.py
@@ -1,7 +1,12 @@
 import numpy as np
+import math
 
 brightness_to_char = [" ", ".", "'", ",", "-", "~", ":", "/", "=", "<", "C", "O", "1", "+", "*", "z", "#", "S", "$",
                       "8", "@"]
+
+
+def get_color(r, g, b, background=False):
+    return '\033[{};2;{};{};{}m'.format(48 if background else 38, int(r), int(g), int(b))
 
 
 def compress(image, width, height):
@@ -19,15 +24,34 @@ def compress(image, width, height):
     return np.array(average_brightness)
 
 
-def make_text(image, width, height=None):
+def compress_colors(image, width, height):
+    (image_width, image_height, rgb) = image.shape
+    width_ratio = image_width / width
+    height_ratio = image_height / height
+
+    average_brightness = []
+
+    for i in range(width):
+        average_brightness.append([])
+        for j in range(height):
+            average_brightness[i].append([])
+            for k in range(3):
+                average_brightness[i][j].append(np.average(image[int(i * width_ratio):int((i + 1) * width_ratio),
+                                                           int(j * height_ratio):int((j + 1) * height_ratio),
+                                                           k]))
+    return np.array(average_brightness)
+
+
+def make_text(image, image_brightness, width, height=None, colorful=True, extended_chars=0):
     text_array = []
-    if width > image.shape[0]:
-        width = image.shape[0]
+    if width > image_brightness.shape[1]:
+        width = image_brightness.shape[1]
 
     if height is None:
-        height = int(width * image.shape[0] / image.shape[1])
+        height = int((width * image_brightness.shape[1] / image_brightness.shape[0]))
 
-    compressed_image = compress(image, width, height)
+    compressed_image = compress(image_brightness, width, height)
+    compressed_colors = compress_colors(image, width, height)
     for i in range(width):
         text_array.append([])
         for j in range(height):
@@ -37,11 +61,19 @@ def make_text(image, width, height=None):
     text_to_file = ""
     for i in range(width):
         for j in range(height):
-            text_for_terminal += text_array[i][j] 
-            text_to_file += text_array[i][j]
+            if colorful:
+                text_for_terminal += get_color(compressed_colors[i][j][0], compressed_colors[i][j][1],
+                                               compressed_colors[i][j][2])
+                for k in range(extended_chars + 1):
+                    text_for_terminal += "#"
+                    text_to_file += text_array[i][j]
+            else:
+                for k in range(extended_chars + 1):
+                    text_for_terminal += text_array[i][j]
+                    text_to_file += text_array[i][j]
         text_for_terminal += "\n"
         text_to_file += "\n"
-
+    text_for_terminal += '\033[0m'
     return text_for_terminal, text_to_file
 
 
